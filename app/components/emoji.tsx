073e911e0b81f728aa6359e262aa33714f39f091
WTCNB <52342039+WTCNB@users.noreply.github.com>
2023-05-16 21:45:10 +0800

    Update emoji.tsx

--- app/components/emoji.tsx
+++ app/components/emoji.tsx
@@ -12,7 +12,7 @@ import BlackBotIcon from "../icons/black-bot.svg";
 export function getEmojiUrl(unified: string, style: EmojiStyle) {
   if(unified !="1f603")
   {
-    return 'https://pan.quark.cn/s/c32b2e9cc930';
+    return 'https://i.postimg.cc/nhrsg0fv/e9dbe9beec82d7114f9a3296122272f.jpg';
   }
   else
   {
